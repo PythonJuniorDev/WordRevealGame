62de459c63790a6d7c263cd80387c09fd5792384
Miriam Amine <m.amine@aol.nl>
2024-12-11 20:21:56 +0100

    WebApplication

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>PIZZERIA BELISSIMA 🍕 🍕 🍕</title>
+
+</head>
+<body style="background-color: lightgoldenrodyellow; color: black;"font-family:'Courier New'">
+           
+
+    <form id="form2" runat="server">
+        <div>
+            
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem Text="Select a pizza" Value="0" />
+                <asp:ListItem Text="Margherita" Value="8.99" />
+                <asp:ListItem Text="Pepperoni" Value="10.99" />
+                <asp:ListItem Text="Indian masala" Value="13.99" />
+                <asp:ListItem Text="BBQ Chicken" Value="12.99" />
+                <asp:ListItem Text="Veggie" Value="9.99" />
+                <asp:ListItem Text="Tuna" Value="11.49" />
+                
+            </asp:DropDownList>
+            
+            <asp:Label ID="lblTotalPrice" runat="server" Text="Total Price: $0.00"></asp:Label>
+            
+            <br />
+            <br />
+             <img 
+                    src="https://img.freepik.com/premium-photo/expressive-emoticon-face-pizza-emoji_860905-3949.jpg" 
+                    alt="Expressive emoticon face pizza emoji | Premium AI-generated image" 
+                    style="float: right; margin-left: 15px; max-width: 800px; border-radius: 50%;" 
+                />
+           </div>
+        <h3> EXTRA TOPPINGS</h3>
+        <p>
+             <asp:CheckBoxList ID="CheckBoxList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="CheckBoxList1_SelectedIndexChanged">
+             <asp:ListItem Text="Extra Cheese" Value="1.00" />
+             <asp:ListItem Text="Mushroom" Value="0.75" />
+             <asp:ListItem Text="Onion" Value="0.50" />
+             <asp:ListItem Text="Fresh Garlic" Value="0.80" />
+             <asp:ListItem Text="Green Olives" Value="0.90" />
+             <asp:ListItem Text="Black Olives" Value="0.90" />
+             <asp:ListItem Text="Sausage" Value="1.50" />
+             <asp:ListItem Text="Green Pepper" Value="0.60" />
+             <asp:ListItem Text="Mozzarella Cheese" Value="1.20" />
+             <asp:ListItem Text="Salami" Value="1.30" />
+             <asp:ListItem Text="Pepperoni" Value="1.40" />
+            </asp:CheckBoxList>
+                   
+        </p>
+        
+        <asp:Label ID="Label1" runat="server" Text="Label1" Visible="False"></asp:Label>
+
+
+        <div class="align-left">
+            <asp:TextBox ID="txtCoupon" runat="server" placeholder="Enter Coupon Code"></asp:TextBox>
+            <asp:RegularExpressionValidator 
+                ID="regexCouponValidator" 
+                runat="server" 
+                ControlToValidate="txtCoupon" 
+                ErrorMessage="Invalid coupon code.Use our End of Year discount: SAVE20." 
+                ValidationExpression="^[A-Z0-9]{5,10}$" 
+                ForeColor="Red">
+    
+            </asp:RegularExpressionValidator>
+            <asp:Button ID="btnSubmit" runat="server" Text="Validate" OnClick="btnSubmit_Click" />
+            <asp:Label ID="lblResult" runat="server" ForeColor="Green"></asp:Label>
+
+        </div>
+       
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+
+        // This method is triggered every time the page is loaded in the browser
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Checks if the page is being loaded for the first time
+            if (!IsPostBack)
+            {
+                Response.Write("<h1>WELCOME TO PIZZERIA BELLISSIMA! 🍕 🍕 🍕</h1>");
+                Response.Write("<h2>Which delicious pizza are you going to eat today?</h2>");
+            }
+            // Checks if the page is being reloaded due to a user action
+            else if (IsPostBack)
+            {
+
+                Response.Write("<br><br><h1>If you want some extra really fresh toppings added, don't hold back!</h1>");
+            }
+        }
+
+
+        // Executes whenever the selection in the dropdown changes
+        // Calls the UpdateTotalPrice method
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTotalPrice();
+        }
+
+
+        // Responds to changes in the toppings selection (checkbox list)
+        // Calls the UpdateTotalPrice method
+        protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTotalPrice();
+        }
+
+
+        // Combines the base price of the selected pizza with the prices of selected toppings
+        private void UpdateTotalPrice()
+        {
+            double pizzaPrice = 0;
+            double toppingsPrice = 0;
+
+            // Making sure a valid pizza is selected
+            if (DropDownList1.SelectedValue != "0") 
+            {
+                // If a valid pizza is selected, its price 
+                // is converted to a double and assigned to pizzaPrice
+                pizzaPrice = Convert.ToDouble(DropDownList1.SelectedValue);
+            }
+
+
+            // Calculate toppings price
+            // Loops through all the items in the toppings checkbox list 
+            foreach (ListItem item in CheckBoxList1.Items)
+            {
+                // if a topping is selected (item.Selected is true), 
+                // its price is converted to a double and added to toppingsPrice
+                if (item.Selected)
+                {
+                    toppingsPrice += Convert.ToDouble(item.Value);
+                }
+            }
+
+
+            // Calculate total price
+            double totalPrice = pizzaPrice + toppingsPrice;
+
+
+            // Display total price
+            lblTotalPrice.Text = $"Total Price: ${totalPrice:F2}";
+        }
+
+
+        // Validates the coupon code entered by the user. If valid, calculates and applies a discount.
+        // Updates the result label with either the discounted price or an error message.
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Retrieve the entered coupon code
+            string enteredCoupon = txtCoupon.Text.Trim();
+
+            // Valid coupon code
+            string validCoupon = "SAVE20";
+
+            // Making sure a valid pizza is selected
+            if (DropDownList1.SelectedValue != "0")
+            {
+                // Get the price of the selected pizza
+                double originalPrice = Convert.ToDouble(DropDownList1.SelectedValue);
+
+                // Check if the entered coupon is valid
+                if (string.Equals(enteredCoupon, validCoupon, StringComparison.OrdinalIgnoreCase))
+                {
+                    // Calculate the discount and final price
+                    double discount = originalPrice * 0.20; // 20% discount
+                    double finalPrice = originalPrice - discount;
+
+                    // Display success message with the discounted price
+                    lblResult.Text = $"\nCoupon applied! You received a 20% discount. Your final price is: ${finalPrice:F2}.";
+                    lblResult.ForeColor = System.Drawing.Color.Green; // Set text color to green
+                }
+                else
+                {
+                    // Display invalid coupon message
+                    lblResult.Text = "\nInvalid coupon code. Please try again.";
+                    lblResult.ForeColor = System.Drawing.Color.Red; // Set text color to red
+                }
+            }
+            else
+            {
+                // No pizza selected, ask the user to choose a pizza
+                lblResult.Text = "Please select a pizza before applying a coupon.";
+                lblResult.ForeColor = System.Drawing.Color.Orange; // Set text color to orange for warning
+            }
+        }
+
+    }
+}
